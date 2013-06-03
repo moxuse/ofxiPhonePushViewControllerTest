260d0998d71a4f18de1f678cb183dc782adc8312
moxuse <moxuse@gmail.com>
2013-06-03 15:31:21 +0900

    added project

---
+++ src/main.mm
@@ -0,0 +1,8 @@
+#include "ofMain.h"
+#include "testApp.h"
+
+int main(){
+	ofSetupOpenGL(1024,768, OF_FULLSCREEN);			// <-------- setup the GL context
+    ofPtr<ofBaseApp> ptr = ofPtr<ofBaseApp>(new testApp());
+    ofRunApp(ptr);
+}
---
+++ src/testApp.mm
@@ -0,0 +1,98 @@
+#include "testApp.h"
+
+static dispatch_once_t onceToken;
+
+//--------------------------------------------------------------
+void testApp::setup(){
+	ofxAccelerometer.setup();
+    
+    ofRegisterTouchEvents(this);
+
+    ofxiPhoneAlerts.addListener(this);
+    
+    subViewController = [[UIViewController alloc] init];
+    
+    subViewController.title = @"Second View Controller";
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+    
+    dispatch_once(&onceToken, ^{
+        UIWindow * window = ofxiPhoneGetUIWindow();
+        
+        UIViewController *firstViewController = ofxiPhoneGetViewController();
+        
+        firstViewController.title = @"First View Controller";
+        
+        window.rootViewController = subViewController;
+        
+        navigationController = [[UINavigationController alloc] initWithRootViewController:firstViewController];
+        
+        window.rootViewController = navigationController;
+    });
+    
+    ofSetColor(0, 0, 0);
+    ofDrawBitmapString("This is ofxiPhoneGLView.. \nTouch and Push viewController", 10, ofGetHeight() / 2 - 50 );
+}
+
+//--------------------------------------------------------------
+void testApp::exit(){
+
+}
+
+//--------------------------------------------------------------
+void testApp::touchDown(ofTouchEventArgs & touch){
+
+}
+
+//--------------------------------------------------------------
+void testApp::touchMoved(ofTouchEventArgs & touch){
+
+}
+
+//--------------------------------------------------------------
+void testApp::touchUp(ofTouchEventArgs & touch){
+    
+    /*
+    touch window to push viewController..
+    */
+    
+    [navigationController pushViewController:subViewController animated:YES];
+}
+
+//--------------------------------------------------------------
+void testApp::touchDoubleTap(ofTouchEventArgs & touch){
+
+}
+
+//--------------------------------------------------------------
+void testApp::touchCancelled(ofTouchEventArgs & touch){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::lostFocus(){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotFocus(){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotMemoryWarning(){
+
+}
+
+//--------------------------------------------------------------
+void testApp::deviceOrientationChanged(int newOrientation){
+
+}
+
